391ed1d3e1866bdfd8023fe0e072ba213725ee89
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-04-17 02:19:24 +0300

    tyday

--- index.js
+++ index.js
@@ -7,12 +7,40 @@ const PORT = process.env.PORT || 6000
 const router = require('./routes/router')
 const models = require('./models/models')
 const swaggerUi = require('swagger-ui-express');
-const fs = require("fs")
-const YAML = require('yaml')
-const file  = fs.readFileSync('./swagger.yaml', 'utf8')
+const swaggerJsdoc = require('swagger-jsdoc');
 
-const swaggerDocument = YAML.parse(file)
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const options = {
+    swaggerDefinition:{
+        openapi: '3.0.0',
+        info:{
+            title: 'Swagger',
+            version:'1.0.0',
+            description: 'My first Api'
+        },
+        servers:[
+            { url: 'https://mongo-users.vercel.app'}
+        ],
+        components:{
+            schemas:{
+                User:{
+                    type: 'object',
+                    properties:{
+                        id:{
+                            type: integer,
+                        },
+                        name:{
+                            type: string,
+                     
+                        } 
+                    }
+                }
+            }
+        }
+    },
+    apis:[router]
+};
+const specs = swaggerJsdoc(options)
+app.use('/',swaggerUi.serve, swaggerUi.setup(specs))
 app.use(cors())
 app.use(express.json())
 
