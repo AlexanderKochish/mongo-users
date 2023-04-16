421932a8639c74bc952dccc57bced47e5b54f5d9
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-04-16 23:53:25 +0300

    tree commit

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 6000
 const router = require('./routes/router')
 const models = require('./models/models')
+const swaggerUi = require('swagger-ui-express');
+const fs = require("fs")
+const YAML = require('yaml')
+
+const file  = fs.readFileSync('./swagger.yaml', 'utf8')
+const swaggerDocument = YAML.parse(file)
 
 app.use(cors())
 app.use(express.json())
 mongoose.connect(`mongodb+srv://shvepsolek:${process.env.DB_PASSWORD}@cluster0.kbbfhuo.mongodb.net/mongo-users?retryWrites=true&w=majority`)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/",router)
 
