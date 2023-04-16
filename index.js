7b5ade60daa582dc11b8d525ab596c450ef39e19
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-04-17 01:52:06 +0300

    ulolopot

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 6000
 const router = require('./routes/router')
 const models = require('./models/models')
-// const swaggerUi = require('swagger-ui-express');
-// const fs = require("fs")
-// const YAML = require('yaml')
+const swaggerUi = require('swagger-ui-express');
+const fs = require("fs")
+const YAML = require('yaml')
+const file  = fs.readFileSync('./swagger.yaml', 'utf8')
+
 
 app.use(cors())
 app.use(express.json())
 app.use("/",router)
 
+
+
 mongoose.connect(`mongodb+srv://shvepsolek:${process.env.DB_PASSWORD}@cluster0.kbbfhuo.mongodb.net/mongo-users?retryWrites=true&w=majority`)
-// const file  = fs.readFileSync('./swagger.yaml', 'utf8')
-// const swaggerDocument = YAML.parse(file)
-// app.get("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const swaggerDocument = YAML.parse(file)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => console.log(`Server run on port ${PORT}`))
