647bce2ab935cb726646a66e4d7a72ff97dafea2
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-04-17 01:59:24 +0300

    ionic

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
+const app = express()
 require('dotenv').config()
 const cors = require('cors')
-const app = express()
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 6000
 const router = require('./routes/router')
